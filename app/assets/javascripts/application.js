226f4ca7d2ec8c9a0aa6fb330e270f95152137b0
mzxxgs <mzx.xgs@gmail.com>
2016-12-26 09:47:23 +0800

    user can login/logout/signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks      
+//= require turbolinks
 //= require bootstrap/alert
+//= require bootstrap/dropdown
 //= require_tree .
