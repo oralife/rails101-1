40a7716c06029b34abb3ad053c13d45a6a19fe3e
mzxxgs <mzx.xgs@gmail.com>
2016-12-26 13:33:40 +0800

    join & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :groups do
     resources :posts
+
+    member do
+      post :join
+      post :quit
+    end
   end
   root 'groups#index'
 end
